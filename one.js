c2c4f930215c2c69ffe28d67e8b77045f1604a70
shinzn <42233998+shinzn@users.noreply.github.com>
2018-08-16 16:05:47 +0800

    Update one.js

--- one.js
+++ one.js
@@ -21,7 +21,7 @@ const txb = new bitcoin.TransactionBuilder(regtest);
 
 
 //用listunspent 取出最後一筆資料的txid
-txb.addInput('c3acac43c6f0e83538fb5c82ed01379349f5dccf43a8af7bb5d8f0c60fb1fcfc', 0);
+txb.addInput('07de807e238e5aad1ee42e5240ea483373e08a589f89fec0ab692245c73e89fe', 0);
 
 //用getnewaddress取得新的位址
 txb.addOutput('2MxfNS8sB1GcCo3GBLmX2PUw277H9zvrn2L', 12)
